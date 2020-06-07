ff67fb573e256d8cd0fbe03dcf9a1a6dda351365
Jack Madden <jck13mad@gmail.com>
2020-06-07 19:48:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
     @breed = params[:breed]
     @age = params[:age]
     
-    @puppy = 
+    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
     
     erb :display_puppy
   end 
